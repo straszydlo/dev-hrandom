8b15778dd65cde6a3f171a47974d094ce2f999bf
straszydlo <kamil.szydlo.ks@gmail.com>
2022-02-14 15:33:49 +0100

    Function for generating new bits

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,11 @@
 module Main where
 
+import Control.Monad.State
 import Data.Char
 import Lib
 
 charSize :: Int
 charSize = 16
-
-type BinDigit = Bool
-type BinNumber = [BinDigit]
-type BinStream = BinNumber
 
 bits :: BinStream
 bits = cycle [False, False, False, False, False, False, False, False, False, True, False, False, False, False, False, True] --infinite stream of 65s encoded in a pseudo-binary fashion, to be replaced with real implementation
@@ -16,9 +13,6 @@ bits = cycle [False, False, False, False, False, False, False, False, False, Tru
 grp :: Int -> [a] -> [[a]]
 grp _ [] = []
 grp n list = (take n list):(grp n (drop n list))
-
-toDecimal :: BinNumber -> Int
-toDecimal = foldl (\acc elem -> 2*acc + if elem then 1 else 0) 0
 
 binChars :: BinStream -> String
 binChars = map (chr . toDecimal) . grp charSize
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,36 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Control.Monad.State
+
+type BinDigit = Bool
+type BinNumber = [BinDigit]
+type BinStream = BinNumber
+
+toDecimal :: BinNumber -> Int
+toDecimal = foldl (\acc elem -> 2*acc + if elem then 1 else 0) 0
+
+nextBitS :: State BinNumber BinDigit
+nextBitS = do
+  current <- get
+  let (firstHalf, secondHalf) = splitAt 32 current
+  let exponent = toDecimal firstHalf
+  let next = modularExp exponent
+  let newFirstHalf = drop 32 $ toBinary next
+  put $ newFirstHalf ++ secondHalf
+  let newBit = foldr xor False (zipWith (&&) firstHalf secondHalf)
+  return newBit
+  where modularExp :: Int -> Int
+        modularExp 0 = 1
+        modularExp exponent = (base * modularExp (exponent - 1)) `mod` modulus
+        modulus = 2137 --completely arbitrary prime
+        base = 10 --smallest generator for Z2137 cyclic group
+        xor a b = (a || b) && not (a && b)
+
+toBinary :: Int -> BinNumber
+toBinary = pad . reverse . reversedDigits
+  where reversedDigits 0 = [False]
+        reversedDigits 1 = [True]
+        reversedDigits n = (n `mod` 2 == 1):(reversedDigits (n `div` 2))
+        pad :: [BinDigit] -> [BinDigit]
+        pad digits = replicate (64 - length digits) False ++ digits
+
